02343a4cd9dcdea0dc6c0d6c8a3a244036400b05
Robert Kerr <rskerr@gmail.com>
2014-01-31 11:25:11 -0500

    Completed solution for e1.12, added solutions for e1.14 & e1.15

--- src/sicp_clojure/e1.12.clj
+++ src/sicp_clojure/e1.12.clj
@@ -13,4 +13,55 @@
 ;  that computes elements of Pascal’s triangle by means of a recursive process.
 ;
 
+;; For computation, we can consider the triangle to look like so:
 ;;
+;;               1
+;;               1   1
+;;               1   2   1
+;;               1   3   3   1
+;;               1   4   6   4   1
+;;
+;; In this configuration, the row indicates the 'depth' paramenter
+;; starting at 1, and the nth item is the 'element' we're calculating.
+;;
+;; With that, we can write a recursive method that calculates a single
+;; element when passed a 'depth' and 'element' to calculate.
+;;
+(defn pascals-elements [depth element]
+   (cond (> element depth) nil
+         (< element 1) nil
+         (< depth 1) nil
+         (= depth 1) 1
+         (= element 1) 1
+         (= element depth) 1
+         :else (+ (pascals-elements (- depth 1) (- element 1))
+                  (pascals-elements (- depth 1) element))))
+
+;; Using the function, we can evaluate the pyramid shown above like so:
+(pascals-elements 1 1)
+
+(pascals-elements 2 1)
+(pascals-elements 2 2)
+
+(pascals-elements 3 1)
+(pascals-elements 3 2)
+(pascals-elements 3 3)
+
+
+(pascals-elements 4 1)
+(pascals-elements 4 2)
+(pascals-elements 4 3)
+(pascals-elements 4 4)
+
+(pascals-elements 5 1)
+(pascals-elements 5 2)
+(pascals-elements 5 3)
+(pascals-elements 5 4)
+(pascals-elements 5 5)
+
+;; And some edge cases
+(pascals-elements 0 0)
+(pascals-elements 1 0)
+(pascals-elements 1 -1)
+(pascals-elements 2 0)
+(pascals-elements 2 3)
